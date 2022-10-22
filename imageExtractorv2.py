45176878efa3683bb1ebb04581ef87182ba85eb7
kathir2602 <kathir@crayond.co>
2022-10-22 11:39:56 +0530

    feat:path finder added

--- imageExtractorv2.py
+++ imageExtractorv2.py
@@ -13,9 +13,6 @@ COMPRESSED_OUTPUT_PATH = 'compressed'
 BYTE_CONSTANT = 1024.0
 EXCEL_PATH = 'sample.xlsx'
 
-# creating data frame
-# importing excel file
-
 
 def FetchUrlFromExcel(path):
     df = pd.read_excel(path)
@@ -37,7 +34,6 @@ def compressImages(folder_name):
         if not (os.path.exists(COMPRESSED_OUTPUT_PATH)):
             os.mkdir(COMPRESSED_OUTPUT_PATH)
         files = os.listdir(folder_name)
-        # check Files Through Directory
         for file in files:
             filepath = pathFinder(folder_name,file)
             dist_path = pathFinder(COMPRESSED_OUTPUT_PATH,file)
@@ -47,8 +43,6 @@ def compressImages(folder_name):
                 im1.save(dist_path, "JPEG", quality=12)
             else:
                 shutil.copyfile(filepath, dist_path)
-
-    # if folder exists with that name, ask another name
     except:
         raise Exception()
 
@@ -60,69 +54,41 @@ def pathFinder(path,file):
     except:
        pass
 
-# CREATE FOLDER
+
 def folder_create(name):
     try:
-        # folder creation
+
         if not (os.path.exists(name)):
             os.mkdir(name)
-    # if folder exists with that name, ask another name
     except:
         pass
 def remove_folder(name):
     try:
-        # folder creation
         if (os.path.exists(name)):
             shutil.rmtree(name)
-    # if folder exists with that name, ask another name
     except:
         pass
 
 
-# DOWNLOAD ALL IMAGES FROM THAT URL
+
 def download_images(images, folder_name):
-
-    # initial count is zero
     count = 0
-
-    # print total images found in URL
     print(f"Total {len(images)} Image Found!")
-
-    # checking if images is not zero
     if len(images) != 0:
         for i, image in enumerate(images):
-            # From image tag ,Fetch image Source URL
-            # 1.data-srcset
-            # 2.data-src
-            # 3.data-fallback-src
-            # 4.src
-            # Here we will use exception handling
-            # first we will search for "data-srcset" in img tag
             try:
-                # In image tag ,searching for "data-srcset"
                 image_link = image["data-srcset"]
-
-            # then we will search for "data-src" in img
-            # tag and so on..
             except:
                 try:
-                    # In image tag ,searching for "data-src"
                     image_link = image["data-src"]
                 except:
                     try:
-                        # In image tag ,searching for "data-fallback-src"
                         image_link = image["data-fallback-src"]
                     except:
                         try:
-                            # In image tag ,searching for "src"
                             image_link = image["src"]
-
-                        # if no Source URL found
                         except:
                             pass
-
-            # After getting Image Source URL
-            # We will try to get the content of image
             try:
                 if not image_link == '' and re.match(r'(https?://[^\s]+)', image_link):
                     r = requests.get(image_link).content
@@ -130,52 +96,30 @@ def download_images(images, folder_name):
                     file_ext = {True: file_ext, False: "jpg"}[
                         file_ext in ["png", "svg", "jpeg", "jpg", "gif"]]
                     try:
-                        # possibility of decode
                         r = str(r, 'utf-8')
                     except UnicodeDecodeError:
                         file_name = str(time.time()) + "." + file_ext
                         filepath = pathFinder(folder_name,file_name)
-                        # After checking above condition, Image Download start
                         with open(fr'{filepath}', "wb+") as f:
                             f.write(r)
-
-                        # counting number of image downloaded
                         count += 1
             except:
-                raise
-
-        # There might be possible, that all
-        # images not download
-        # if all images download
+                raise 
         if count == len(images):
             print("All Images Downloaded!")
-
-        # if all images not download
         else:
             print(f"Total {count} Images Downloaded Out of {len(images)}")
 
-# MAIN FUNCTION START
-
 
 def main(urls):
-    # Call folder create function
     folder_create(IMAGE_EXTRACT_PATH)
     for url in urls:
-
-        # content of URL
         r = requests.get(url)
-
-        # Parse HTML Code
         soup = BeautifulSoup(r.text, 'html.parser')
-
-        # find all images in URL
         images = soup.findAll('img')
-
-        # image downloading start
         download_images(images, IMAGE_EXTRACT_PATH)
         compressImages(IMAGE_EXTRACT_PATH)
     remove_folder(IMAGE_EXTRACT_PATH)
 
 
-# CALL MAIN FUNCTION
 main(FetchUrlFromExcel(EXCEL_PATH))
